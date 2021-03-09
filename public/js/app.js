0e242d77d27566dbcfcf888b0b8ad9af59a9fe11
System Administrator <root@Honey.local>
2021-03-08 18:12:48 -0600

    Remove unnecessary console log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,3 @@
-console.log("Client side Javascript file loaded")
-
-// const forecastFn = async (address) => {
-//     await fetch(`http://localhost:3000/weather?address=${address}`).then((res) => {
-//         res.json().then((data) => {
-//             if (data.error) {
-//                 console.log(data.error)
-//             } else {
-//                 console.log(data.forecast)
-//                 console.log(data.location)
-//             }
-//         })
-//     })
-// }
-
 const weatherForm = document.querySelector('form')
 const searchEl = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
